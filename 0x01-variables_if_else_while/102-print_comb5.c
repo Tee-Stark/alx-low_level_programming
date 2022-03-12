645cd9f7e5fd7220562aae605cc59020c0d909d6
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-03-12 22:46:17 +0100

    updating advanced tasks

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,48 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * this code uses putchar function to print
+ * possible combinations of double 2 digit numbers
+ * numbers must be seperate by , and a space
+ * Numbers should be printed in ascending order
+ * You can only use putchar eight times maximum in your code
+ * starting from 0
+ * Return: 0
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int i, j, k, l, m, n;
 
-	while (p < 58)
+
+	for (i = 0; i < 100 ; i++)
 	{
-		while (q < 58)
+		j = i / 10;
+		k = i % 10;
+
+		for (l = 0 ; l < 100; l++)
 		{
-			while (y < 58)
+			m = l / 10;
+			n = l % 10;
+			if (j < m || (j == m && k < n))
 			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
+				putchar(j + '0');
+				putchar(k + '0');
+				putchar(32);
+				putchar(m + '0');
+				putchar(n + '0');
+
+				if (!(j == 9 && k == 8))
+					putchar(44);
+				putchar(32);
 			}
-			q++;
-			q = p;
-			x = q + 1;
+
+
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
+
+
 	}
+
 	putchar('\n');
 	return (0);
+
 }
